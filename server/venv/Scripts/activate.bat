5926405f1a8b37a292ed890178ba07aabe82d7ac
edwinlin6 <edwinzhanminglin@gmail.com>
2023-10-27 00:21:48 -0700

    setup backend

--- server/venv/Scripts/activate.bat
+++ server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=\\wsl.localhost\Ubuntu\home\edwin\AIEducate\server\venv
+set VIRTUAL_ENV=C:\Users\edwin\Documents\AIEducate\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
